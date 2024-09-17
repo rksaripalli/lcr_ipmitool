ipmitool/ipmitool

commit 6e037d6bfbbb93b349c8ca331ebde03a837f76bf

Refactor string comparisons
Clean up use of strcmp/strncmp/strncasecmp for command line arguments.
Never use anything but `strcmp()` unless absolutely neccessary.

Partialy resolves ipmitool/ipmitool#104

lib/ipmi_sdr.c
@@ -4570,8 +4570,8 @@ ipmi_sdr_print_type(struct ipmi_intf *intf, char *type)
 	uint8_t sensor_type = 0;
 
 	if (!type ||
-	    strncasecmp(type, "help", 4) == 0 ||
-	    strncasecmp(type, "list", 4) == 0) {
+	    strcasecmp(type, "help") == 0 ||
+	    strcasecmp(type, "list") == 0) {
 		printf("Sensor Types:\n");
 		for (x = 1; x < SENSOR_TYPE_MAX; x += 2) {
 			printf("\t%-25s (0x%02x)   %-25s (0x%02x)\n",
@@ -4581,7 +4581,7 @@ ipmi_sdr_print_type(struct ipmi_intf *intf, char *type)
 		return 0;
 	}
 
-	if (strncmp(type, "0x", 2) == 0) {
+	if (strcmp(type, "0x") == 0) {
 		/* begins with 0x so let it be entered as raw hex value */
 		if (str2uchar(type, &sensor_type) != 0) {
 			lprintf(LOG_ERR,
@@ -4591,9 +4591,7 @@ ipmi_sdr_print_type(struct ipmi_intf *intf, char *type)
 		}
 	} else {
 		for (x = 1; x < SENSOR_TYPE_MAX; x++) {
-			if (strncasecmp(sensor_type_desc[x], type,
-					__maxlen(type,
-						 sensor_type_desc[x])) == 0) {
+			if (strcasecmp(sensor_type_desc[x], type) == 0) {
 				sensor_type = x;
 				break;
 			}
@@ -4640,8 +4638,8 @@ ipmi_sdr_print_entity(struct ipmi_intf *intf, char *entitystr)
 	int rc = 0;
 
 	if (!entitystr ||
-	    strncasecmp(entitystr, "help", 4) == 0 ||
-	    strncasecmp(entitystr, "list", 4) == 0) {
+	    strcasecmp(entitystr, "help") == 0 ||
+	    strcasecmp(entitystr, "list") == 0) {
 		print_valstr_2col(entity_id_vals, "Entity IDs", -1);
 		return 0;
 	}
@@ -4656,8 +4654,7 @@ ipmi_sdr_print_entity(struct ipmi_intf *intf, char *entitystr)
 
 			/* now try string input */
 			for (i = 0; entity_id_vals[i].str; i++) {
-				if (strncasecmp(entitystr, entity_id_vals[i].str,
-						__maxlen(entitystr, entity_id_vals[i].str)) == 0) {
+				if (strcasecmp(entitystr, entity_id_vals[i].str) == 0) {
 					entity.id = entity_id_vals[i].val;
 					entity.instance = 0x7f;
 					j=1;
@@ -4746,36 +4743,36 @@ ipmi_sdr_main(struct ipmi_intf *intf, int argc, char **argv)
 
 	if (argc == 0)
 		return ipmi_sdr_print_sdr(intf, 0xfe);
-	else if (strncmp(argv[0], "help", 4) == 0) {
+	else if (strcmp(argv[0], "help") == 0) {
 		printf_sdr_usage();
-	} else if (strncmp(argv[0], "list", 4) == 0
-		   || strncmp(argv[0], "elist", 5) == 0) {
+	} else if (strcmp(argv[0], "list") == 0
+		   || strcmp(argv[0], "elist") == 0) {
 
-		if (strncmp(argv[0], "elist", 5) == 0)
+		if (strcmp(argv[0], "elist") == 0)
 			sdr_extended = 1;
 		else
 			sdr_extended = 0;
 
 		if (argc <= 1)
 			rc = ipmi_sdr_print_sdr(intf, 0xfe);
-		else if (strncmp(argv[1], "all", 3) == 0)
+		else if (strcmp(argv[1], "all") == 0)
 			rc = ipmi_sdr_print_sdr(intf, 0xff);
-		else if (strncmp(argv[1], "full", 4) == 0)
+		else if (strcmp(argv[1], "full") == 0)
 			rc = ipmi_sdr_print_sdr(intf,
 						SDR_RECORD_TYPE_FULL_SENSOR);
-		else if (strncmp(argv[1], "compact", 7) == 0)
+		else if (strcmp(argv[1], "compact") == 0)
 			rc = ipmi_sdr_print_sdr(intf,
 						SDR_RECORD_TYPE_COMPACT_SENSOR);
-		else if (strncmp(argv[1], "event", 5) == 0)
+		else if (strcmp(argv[1], "event") == 0)
 			rc = ipmi_sdr_print_sdr(intf,
 						SDR_RECORD_TYPE_EVENTONLY_SENSOR);
-		else if (strncmp(argv[1], "mcloc", 5) == 0)
+		else if (strcmp(argv[1], "mcloc") == 0)
 			rc = ipmi_sdr_print_sdr(intf,
 						SDR_RECORD_TYPE_MC_DEVICE_LOCATOR);
-		else if (strncmp(argv[1], "fru", 3) == 0)
+		else if (strcmp(argv[1], "fru") == 0)
 			rc = ipmi_sdr_print_sdr(intf,
 						SDR_RECORD_TYPE_FRU_DEVICE_LOCATOR);
-		else if (strncmp(argv[1], "generic", 7) == 0)
+		else if (strcmp(argv[1], "generic") == 0)
 			rc = ipmi_sdr_print_sdr(intf,
 						SDR_RECORD_TYPE_GENERIC_DEVICE_LOCATOR);
 		else if (strcmp(argv[1], "help") == 0) {
@@ -4793,35 +4790,35 @@ ipmi_sdr_main(struct ipmi_intf *intf, int argc, char **argv)
 				argv[0]);
 			return (-1);
 		}
-	} else if (strncmp(argv[0], "type", 4) == 0) {
+	} else if (strcmp(argv[0], "type") == 0) {
 		sdr_extended = 1;
 		rc = ipmi_sdr_print_type(intf, argv[1]);
-	} else if (strncmp(argv[0], "entity", 6) == 0) {
+	} else if (strcmp(argv[0], "entity") == 0) {
 		sdr_extended = 1;
 		rc = ipmi_sdr_print_entity(intf, argv[1]);
-	} else if (strncmp(argv[0], "info", 4) == 0) {
+	} else if (strcmp(argv[0], "info") == 0) {
 		rc = ipmi_sdr_print_info(intf);
-	} else if (strncmp(argv[0], "get", 3) == 0) {
+	} else if (strcmp(argv[0], "get") == 0) {
 		rc = ipmi_sdr_print_entry_byid(intf, argc - 1, &argv[1]);
-	} else if (strncmp(argv[0], "dump", 4) == 0) {
+	} else if (strcmp(argv[0], "dump") == 0) {
 		if (argc < 2) {
 			lprintf(LOG_ERR, "Not enough parameters given.");
 			lprintf(LOG_NOTICE, "usage: sdr dump <file>");
 			return (-1);
 		}
 		rc = ipmi_sdr_dump_bin(intf, argv[1]);
-	} else if (strncmp(argv[0], "fill", 4) == 0) {
+	} else if (strcmp(argv[0], "fill") == 0) {
 		if (argc <= 1) {
 			lprintf(LOG_ERR, "Not enough parameters given.");
 			lprintf(LOG_NOTICE, "usage: sdr fill sensors");
 			lprintf(LOG_NOTICE, "usage: sdr fill file <file>");
 			lprintf(LOG_NOTICE, "usage: sdr fill range <range>");
 			return (-1);
-		} else if (strncmp(argv[1], "sensors", 7) == 0) {
+		} else if (strcmp(argv[1], "sensors") == 0) {
 			rc = ipmi_sdr_add_from_sensors(intf, 21);
-		} else if (strncmp(argv[1], "nosat", 5) == 0) {
+		} else if (strcmp(argv[1], "nosat") == 0) {
 			rc = ipmi_sdr_add_from_sensors(intf, 0);
-		} else if (strncmp(argv[1], "file", 4) == 0) {
+		} else if (strcmp(argv[1], "file") == 0) {
 			if (argc < 3) {
 				lprintf(LOG_ERR,
 					"Not enough parameters given.");
@@ -4830,7 +4827,7 @@ ipmi_sdr_main(struct ipmi_intf *intf, int argc, char **argv)
 				return (-1);
 			}
 			rc = ipmi_sdr_add_from_file(intf, argv[2]);
-		} else if (strncmp(argv[1], "range", 4) == 0) {
+		} else if (strcmp(argv[1], "range") == 0) {
 			if (argc < 3) {
 				lprintf(LOG_ERR,
 					"Not enough parameters given.");

lib/helper.c
@@ -647,7 +647,7 @@ uint32_t str2val32(const char *str, const struct valstr *vs)
 	int i;
 
 	for (i = 0; vs[i].str; i++) {
-		if (strncasecmp(vs[i].str, str, __maxlen(str, vs[i].str)) == 0)
+		if (strcasecmp(vs[i].str, str) == 0)
 			return vs[i].val;
 	}
 

lib/ipmi_channel.c
@@ -488,7 +488,7 @@ ipmi_get_channel_cipher_suites(struct ipmi_intf *intf,
 	req.msg.data_len = sizeof(rqdata);
 
 	rqdata[0] = channel;
-	rqdata[1] = ((strncmp(payload_type, "ipmi", 4) == 0)? 0: 1);
+	rqdata[1] = ((strcmp(payload_type, "ipmi") == 0)? 0: 1);
 
 	do {
 		/* Always ask for cipher suite format */
@@ -805,7 +805,7 @@ ipmi_set_user_access(struct ipmi_intf *intf, int argc, char **argv)
 	uint8_t channel = 0;
 	uint8_t priv = 0;
 	uint8_t user_id = 0;
-	if (argc > 0 && strncmp(argv[0], "help", 4) == 0) {
+	if (argc > 0 && strcmp(argv[0], "help") == 0) {
 		printf_channel_usage();
 		return 0;
 	} else if (argc < 3) {
@@ -827,25 +827,25 @@ ipmi_set_user_access(struct ipmi_intf *intf, int argc, char **argv)
 		return (-1);
 	}
 	for (i = 2; i < argc; i ++) {
-		if (strncmp(argv[i], "callin=", 7) == 0) {
-			if (strncmp(argv[i] + 7, "off", 3) == 0) {
+		if (strcmp(argv[i], "callin=") == 0) {
+			if (strcmp(argv[i] + strlen("callin="), "off") == 0) {
 				user_access.callin_callback = 1;
 			} else {
 				user_access.callin_callback = 0;
 			}
-		} else if (strncmp(argv[i], "link=", 5) == 0) {
-			if (strncmp(argv[i] + 5, "off", 3) == 0) {
+		} else if (strcmp(argv[i], "link=") == 0) {
+			if (strcmp(argv[i] + strlen("link="), "off") == 0) {
 				user_access.link_auth = 0;
 			} else {
 				user_access.link_auth = 1;
 			}
-		} else if (strncmp(argv[i], "ipmi=", 5) == 0) {
-			if (strncmp(argv[i] + 5, "off", 3) == 0) {
+		} else if (strcmp(argv[i], "ipmi=") == 0) {
+			if (strcmp(argv[i] + strlen("ipmi="), "off") == 0) {
 				user_access.ipmi_messaging = 0;
 			} else {
 				user_access.ipmi_messaging = 1;
 			}
-		} else if (strncmp(argv[i], "privilege=", 10) == 0) {
+		} else if (strcmp(argv[i], "privilege=") == 0) {
 			if (str2uchar(argv[i] + 10, &priv) != 0) {
 				lprintf(LOG_ERR,
 						"Numeric value expected, but '%s' given.",
@@ -880,10 +880,10 @@ ipmi_channel_main(struct ipmi_intf *intf, int argc, char **argv)
 		lprintf(LOG_ERR, "Not enough parameters given.");
 		printf_channel_usage();
 		return (-1);
-	} else if (strncmp(argv[0], "help", 4) == 0) {
+	} else if (strcmp(argv[0], "help") == 0) {
 		printf_channel_usage();
 		return 0;
-	} else if (strncmp(argv[0], "authcap", 7) == 0) {
+	} else if (strcmp(argv[0], "authcap") == 0) {
 		if (argc != 3) {
 			printf_channel_usage();
 			return (-1);
@@ -893,7 +893,7 @@ ipmi_channel_main(struct ipmi_intf *intf, int argc, char **argv)
 			return (-1);
 		}
 		retval = ipmi_get_channel_auth_cap(intf, channel, priv);
-	} else if (strncmp(argv[0], "getaccess", 10) == 0) {
+	} else if (strcmp(argv[0], "getaccess") == 0) {
 		uint8_t user_id = 0;
 		if ((argc < 2) || (argc > 3)) {
 			lprintf(LOG_ERR, "Not enough parameters given.");
@@ -909,9 +909,9 @@ ipmi_channel_main(struct ipmi_intf *intf, int argc, char **argv)
 			}
 		}
 		retval = ipmi_get_user_access(intf, channel, user_id);
-	} else if (strncmp(argv[0], "setaccess", 9) == 0) {
+	} else if (strcmp(argv[0], "setaccess") == 0) {
 		return ipmi_set_user_access(intf, (argc - 1), &(argv[1]));
-	} else if (strncmp(argv[0], "info", 4) == 0) {
+	} else if (strcmp(argv[0], "info") == 0) {
 		channel = 0xE;
 		if (argc > 2) {
 			printf_channel_usage();
@@ -923,11 +923,11 @@ ipmi_channel_main(struct ipmi_intf *intf, int argc, char **argv)
 			}
 		}
 		retval = ipmi_get_channel_info(intf, channel);
-	} else if (strncmp(argv[0], "getciphers", 10) == 0) {
+	} else if (strcmp(argv[0], "getciphers") == 0) {
 		/* channel getciphers <ipmi|sol> [channel] */
 		channel = 0xE;
 		if ((argc < 2) || (argc > 3) ||
-		    (strncmp(argv[1], "ipmi", 4) && strncmp(argv[1], "sol",  3))) {
+		    (strcmp(argv[1], "ipmi") && strcmp(argv[1], "sol"))) {
 			printf_channel_usage();
 			return (-1);
 		}

lib/ipmi_chassis.c
@@ -1071,7 +1071,7 @@ get_bootparam_options(char *optstring,
 	{NULL}	/* End marker */
 	}, *op;
 
-	if (strncmp(optstring, "options=", 8) != 0) {
+	if (strcmp(optstring, "options=") != 0) {
 		lprintf(LOG_ERR, "No options= keyword found \"%s\"", optstring);
 		return -1;
 	}
@@ -1082,12 +1082,12 @@ get_bootparam_options(char *optstring,
 			optionError = 1;
 			break;
 		}
-		if (strncmp(token, "no-", 3) == 0) {
+		if (strcmp(token, "no-") == 0) {
 			setbit = 1;
 			token += 3;
 		}
 		for (op = options; op->name; ++op) {
-			if (strncmp(token, op->name, strlen(op->name)) == 0) {
+			if (strcmp(token, op->name) == 0) {
 				if (setbit) {
 				    *set_flag |= op->value;
 				} else {
@@ -2046,7 +2046,7 @@ ipmi_chassis_main(struct ipmi_intf * intf, int argc, char ** argv)
 			    unsigned char clr_flag=0;
 				if (!strcmp(argv[2], "help")  ||
 						argc < 4 || (argc >= 4 &&
-							 strncmp(argv[2], "bootflag", 8) != 0)) {
+							 strcmp(argv[2], "bootflag") != 0)) {
 					ipmi_chassis_set_bootflag_help();
 				} else {
 					if (argc == 5) {
@@ -2076,7 +2076,6 @@ ipmi_chassis_main(struct ipmi_intf * intf, int argc, char ** argv)
 			lprintf(LOG_NOTICE, "  floppy: Force boot from Floppy/primary removable media");
 		} else {
 			static const char *kw = "options=";
-			static const int kw_len = 8;
 			char *optstr = NULL;
 			uint8_t flags[BF_BYTE_COUNT];
 			bool use_flags = false;
@@ -2086,8 +2085,8 @@ ipmi_chassis_main(struct ipmi_intf * intf, int argc, char ** argv)
 					/* Exclusive clear-cmos, no other flags */
 					optstr = "clear-cmos";
 				}
-				else if (!strncmp(argv[2], kw, kw_len)) {
-					optstr = argv[2] + kw_len;
+				else if (!strcmp(argv[2], kw)) {
+					optstr = argv[2] + strlen(kw);
 				}
 			}
 			if (optstr) {

lib/ipmi_dcmi.c
@@ -655,7 +655,7 @@ str2val2(const char *str, const struct dcmi_cmd *vs)
 		return 0;
 	}
 	for (i = 0; vs[i].str; i++) {
-		if (strncasecmp(vs[i].str, str, __maxlen(str, vs[i].str)) == 0)
+		if (strcasecmp(vs[i].str, str) == 0)
 			return vs[i].val;
 	}
 	return vs[i].val;
@@ -1295,7 +1295,7 @@ ipmi_dcmi_prnt_setmngctrlids(struct ipmi_intf * intf, uint8_t * data)
 		/* because after call "Set mc id string" RMCP+ will go down
 		 * we have no "rsp"
 		 */
-		if (strncmp(intf->name, "lanplus", 7)) {
+		if (strcmp(intf->name, "lanplus")) {
 			if (chk_rsp(rsp)) {
 				return -1;
 			}
@@ -3690,7 +3690,7 @@ ipmi_dcmi_main(struct ipmi_intf * intf, int argc, char **argv)
 	int i;
 	struct ipmi_rs *rsp;
 
-	if ((argc == 0) || (strncmp(argv[0], "help", 4) == 0)) {
+	if ((argc == 0) || (strcmp(argv[0], "help") == 0)) {
 		print_strs(dcmi_cmd_vals,
 		           "Data Center Management Interface commands",
 		           LOG_ERR, 0);
@@ -3804,7 +3804,7 @@ ipmi_dcmi_main(struct ipmi_intf * intf, int argc, char **argv)
 	{
 		switch (argc) {
 		case 2:
-			if (strncmp(argv[1], "activate_dhcp", 13) != 0) {
+			if (strcmp(argv[1], "activate_dhcp") != 0) {
 				print_strs( dcmi_conf_param_vals,
 				            "DCMI Configuration Parameters",
 				            LOG_ERR, 0);
@@ -3812,14 +3812,14 @@ ipmi_dcmi_main(struct ipmi_intf * intf, int argc, char **argv)
 			}
 			break;
 		default:
-			if (argc != 3 || strncmp(argv[1], "help", 4) == 0) {
+			if (argc != 3 || strcmp(argv[1], "help") == 0) {
 				print_strs(dcmi_conf_param_vals,
 				           "DCMI Configuration Parameters",
 				           LOG_ERR, 0);
 				return -1;
 			}
 		}
-		if (strncmp(argv[1], "activate_dhcp", 13) == 0) {
+		if (strcmp(argv[1], "activate_dhcp") == 0) {
 			rsp = ipmi_dcmi_setconfparam(intf, 1, 1);
 		} else {
 			uint16_t tmp_val = 0;
@@ -3873,7 +3873,7 @@ ipmi_nm_main(struct ipmi_intf * intf, int argc, char **argv)
 {
 	struct nm_discover disc;
 
-	if ((argc == 0) || (strncmp(argv[0], "help", 4) == 0)) {
+	if ((argc == 0) || (strcmp(argv[0], "help") == 0)) {
 		print_strs(nm_cmd_vals,
 		           "Node Manager Interface commands",
 		           LOG_ERR, 0);

lib/ipmi_ekanalyzer.c
@@ -561,7 +561,7 @@ ipmi_ekanalyzer_main(struct ipmi_intf *__UNUSED__(intf), int argc, char **argv)
 			option = argv[argument_offset];
 			index ++;
 			argc--;
-		} else if ( strncmp(&argv[argument_offset][2], "=", 1) == 0) {
+		} else if ( strcmp(&argv[argument_offset][2], "=") == 0) {
 			/* since the command line must receive xx=filename,
 			 * so the position of "=" sign is 2
 			 */
@@ -2729,7 +2729,7 @@ ipmi_ek_display_board_info_area(FILE *input_file, char *board_type,
 		printf("%s: None\n", board_type);
 		goto out;
 	}
-	if (strncmp(board_type, "Custom", 6 ) != 0) {
+	if (strcmp(board_type, "Custom") != 0) {
 		unsigned char *data, *str;
 		unsigned int i = 0;
 		data = malloc(size_board + 1); /* Make room for type/length field */

lib/ipmi_event.c
@@ -232,7 +232,7 @@ ipmi_event_find_offset(struct ipmi_intf *intf, uint8_t sensor_type, uint8_t even
 	for (evt = ipmi_get_first_event_sensor_type(intf, sensor_type, event_type);
 			evt; evt = ipmi_get_next_event_sensor_type(evt)) {
 		if (evt->desc &&
-			strncasecmp(desc, evt->desc, __maxlen(desc, evt->desc)) == 0) {
+			strcasecmp(desc, evt->desc) == 0) {
 			return evt->offset;
 		}
 	}
@@ -270,9 +270,9 @@ ipmi_event_fromsensor(struct ipmi_intf * intf, char * id, char * state, char * e
 
 	if (!evdir)
 		emsg.event_dir = EVENT_DIR_ASSERT;
-	else if (strncasecmp(evdir, "assert", 6) == 0)
+	else if (strcmp(evdir, "assert") == 0)
 		emsg.event_dir = EVENT_DIR_ASSERT;
-	else if (strncasecmp(evdir, "deassert", 8) == 0)
+	else if (strcmp(evdir, "deassert") == 0)
 		emsg.event_dir = EVENT_DIR_DEASSERT;
 	else {
 		lprintf(LOG_ERR, "Invalid event direction %s.  Must be 'assert' or 'deassert'", evdir);
@@ -318,7 +318,7 @@ ipmi_event_fromsensor(struct ipmi_intf * intf, char * id, char * state, char * e
 		int hilo = 0;
 		off = 1;
 
-		if (!state || strncasecmp(state, "list", 4) == 0) {
+		if (!state || strcmp(state, "list") == 0) {
 			printf("Sensor States:\n");
 			printf("  lnr : Lower Non-Recoverable \n");
 			printf("  lcr : Lower Critical\n");
@@ -329,12 +329,12 @@ ipmi_event_fromsensor(struct ipmi_intf * intf, char * id, char * state, char * e
 			return -1;
 		}
 
-		if (0 != strncasecmp(state, "lnr", 3) &&
-		    0 != strncasecmp(state, "lcr", 3) &&
-		    0 != strncasecmp(state, "lnc", 3) &&
-		    0 != strncasecmp(state, "unc", 3) &&
-		    0 != strncasecmp(state, "ucr", 3) &&
-		    0 != strncasecmp(state, "unr", 3))
+		if (0 != strcmp(state, "lnr") &&
+		    0 != strcmp(state, "lcr") &&
+		    0 != strcmp(state, "lnc") &&
+		    0 != strcmp(state, "unc") &&
+		    0 != strcmp(state, "ucr") &&
+		    0 != strcmp(state, "unr"))
 		{
 			lprintf(LOG_ERR, "Invalid threshold identifier %s", state);
 			return -1;
@@ -415,7 +415,7 @@ ipmi_event_fromsensor(struct ipmi_intf * intf, char * id, char * state, char * e
 		/* 
 		 * print list of available states for this sensor
 		 */
-		if (!state || strncasecmp(state, "list", 4) == 0) {
+		if (!state || strcasecmp(state, "list") == 0) {
 			print_sensor_states(intf, emsg.sensor_type, emsg.event_type);
 			printf("Sensor State Shortcuts:\n");
 			for (x = 0; x < sizeof(digi_on)/sizeof(*digi_on); x++) {
@@ -426,12 +426,12 @@ ipmi_event_fromsensor(struct ipmi_intf * intf, char * id, char * state, char * e
 
 		off = 0;
 		for (x = 0; x < sizeof(digi_on)/sizeof(*digi_on); x++) {
-			if (strncasecmp(state, digi_on[x], strlen(digi_on[x])) == 0) {
+			if (strcasecmp(state, digi_on[x]) == 0) {
 				emsg.event_data[0] = 1;
 				off = 1;
 				break;
 			}
-			else if (strncasecmp(state, digi_off[x], strlen(digi_off[x])) == 0) {
+			else if (strcasecmp(state, digi_off[x]) == 0) {
 				emsg.event_data[0] = 0;
 				off = 1;
 				break;
@@ -455,7 +455,7 @@ ipmi_event_fromsensor(struct ipmi_intf * intf, char * id, char * state, char * e
 		/* 
 		 * print list of available states for this sensor
 		 */
-		if (!state || strncasecmp(state, "list", 4) == 0) {
+		if (!state || strcasecmp(state, "list") == 0) {
 			print_sensor_states(intf, emsg.sensor_type, emsg.event_type);
 			return 0;
 		}
@@ -475,7 +475,7 @@ ipmi_event_fromsensor(struct ipmi_intf * intf, char * id, char * state, char * e
 		/* 
 		 * print list of available states for this sensor
 		 */
-		if (!state || strncasecmp(state, "list", 4) == 0) {
+		if (!state || strcasecmp(state, "list") == 0) {
 			print_sensor_states(intf, emsg.sensor_type, emsg.event_type);
 			return 0;
 		}
@@ -596,11 +596,11 @@ ipmi_event_main(struct ipmi_intf * intf, int argc, char ** argv)
 {
 	int rc = 0;
 
-	if (argc == 0 || strncmp(argv[0], "help", 4) == 0) {
+	if (argc == 0 || strcmp(argv[0], "help") == 0) {
 		ipmi_event_usage();
 		return 0;
 	}
-	if (strncmp(argv[0], "file", 4) == 0) {
+	if (strcmp(argv[0], "file") == 0) {
 		if (argc < 2) {
 			ipmi_event_usage();
 			return 0;

rksaripalli/lcr_ipmitool

commit 202f7427e0a4d1f319fc4b914676cc2f08da6c6c

sdr: Refix 6e037d6bfbbb93b349c8ca331ebde03a (#41)
A bug was introduced by commit 6e037d6bfbbb93b349c8ca331ebde03a837f76bf
due to which the command `ipmitool sdr type` stopped accepting raw
hex values for the type and would only accept strings.

Fix that by partially reverting the troublesome commit.

Additionally, apply the logic of that commit to calls of
`strcasecmp()` in ipmi_sdr.c.

Resolves https://codeberg.org/IPMITool/ipmitool/issues/41

Signed-off-by: Alexander Amelkin <alexander@amelkin.msk.ru>

lib/ipmi_sdr.c
@@ -4570,8 +4570,9 @@ ipmi_sdr_print_type(struct ipmi_intf *intf, char *type)
 	uint8_t sensor_type = 0;
 
 	if (!type ||
-	    strcasecmp(type, "help") == 0 ||
-	    strcasecmp(type, "list") == 0) {
+	    !strcasecmp(type, "help") ||
+	    !strcasecmp(type, "list"))
+	{
 		printf("Sensor Types:\n");
 		for (x = 1; x < SENSOR_TYPE_MAX; x += 2) {
 			printf("\t%-25s (0x%02x)   %-25s (0x%02x)\n",
@@ -4581,7 +4582,7 @@ ipmi_sdr_print_type(struct ipmi_intf *intf, char *type)
 		return 0;
 	}
 
-	if (!strcmp(type, "0x")) {
+	if (!strncmp(type, "0x", 2)) {
 		/* begins with 0x so let it be entered as raw hex value */
 		if (str2uchar(type, &sensor_type) != 0) {
 			lprintf(LOG_ERR,
@@ -4591,7 +4592,7 @@ ipmi_sdr_print_type(struct ipmi_intf *intf, char *type)
 		}
 	} else {
 		for (x = 1; x < SENSOR_TYPE_MAX; x++) {
-			if (strcasecmp(sensor_type_desc[x], type) == 0) {
+			if (!strcasecmp(sensor_type_desc[x], type)) {
 				sensor_type = x;
 				break;
 			}
@@ -4638,8 +4639,8 @@ ipmi_sdr_print_entity(struct ipmi_intf *intf, char *entitystr)
 	int rc = 0;
 
 	if (!entitystr ||
-	    strcasecmp(entitystr, "help") == 0 ||
-	    strcasecmp(entitystr, "list") == 0) {
+	    !strcasecmp(entitystr, "help") ||
+	    !strcasecmp(entitystr, "list")) {
 		print_valstr_2col(entity_id_vals, "Entity IDs", -1);
 		return 0;
 	}
@@ -4654,7 +4655,7 @@ ipmi_sdr_print_entity(struct ipmi_intf *intf, char *entitystr)
 
 			/* now try string input */
 			for (i = 0; entity_id_vals[i].str; i++) {
-				if (strcasecmp(entitystr, entity_id_vals[i].str) == 0) {
+				if (!strcasecmp(entitystr, entity_id_vals[i].str)) {
 					entity.id = entity_id_vals[i].val;
 					entity.instance = 0x7f;
 					j=1;